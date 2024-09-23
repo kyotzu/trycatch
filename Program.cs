13240aee72d2d0a9303b4bcd18a7fbb47786c852
kyotzu <67776476+kyotzu@users.noreply.github.com>
2024-09-23 12:42:27 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class MyException : Exception
+{
+    public MyException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Exception[] exceptions = new Exception[]
+        {
+            new ArgumentNullException("Аргумент не может быть null"),
+            new InvalidOperationException("Неверная операция"),
+            new DivideByZeroException("Деление на ноль"),
+            new IndexOutOfRangeException("Индекс вне диапазона"),
+            new MyException("Моё исключение")
+        };
+
+
+        foreach (var ex in exceptions)
+        {
+            try
+            {
+                throw ex;
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine($"исключение: {e.Message}");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine($"исключение: {e.Message}");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"исключение: {e.Message}");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"исключение: {e.Message}");
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine($"исключение: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("finally выполнен.");
+            }
+        }
+    }
+}
+
